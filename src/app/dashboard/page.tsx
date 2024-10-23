2aa71e70bb83bd94397a9e29588da2c027e06268
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2024-10-23 11:43:38 -0300

    feat: styling pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,14 +3,14 @@ import { Arrow } from "@/components/Icons/Arrow"
 
 export default function DashboardPage() {
   return (
-    <section className="container flex flex-col flex-nowrap mx-auto min-h-screen bg-right-bottom bg-no-repeat pt-10 md:pt-24">
+    <section className="container flex flex-col flex-nowrap mx-auto min-h-screen bg-right-bottom bg-no-repeat pt-10 md:pt-24 bg-dashboard-squares bg-[length:467px_463px] md:bg-[url()]" >
       <div className="flex flex-col items-center">
         <Avatar />
         <h1 className="text-4xl mt-6 md:mt-0 md:text-8xl/tight text-center w-[303px] md:w-full">Boas vindas, <span className="font-bold">Fulano de Tal</span></h1>
         <h3 className="text-lg mt-3 md:mt-0 md:text-3xl/tight font-thin text-slate-500">Que tal analisarmos seu GitHub?</h3>
       </div>
 
-      <div className="flex content-start flex-col items-center md:items-start bg-dashboard-squares bg-no-repeat bg-[right_top_50px] md:bg-right-top min-h-[467px] w-full mt-6 mb:mt-10 bg-[length:400px_400px] md:bg-[length:600px_600px] ">
+      <div className="flex content-start flex-col items-center md:items-start md:bg-dashboard-squares bg-no-repeat bg-[right_top_50px] md:bg-right-top min-h-[467px] w-full mt-6 mb:mt-10 md:bg-[length:600px_600px] ">
         <ul className="w-2/3 md:w-1/2 mx-auto md:mx-0 bg-white">
           <li className="border-t border-slate-300 flex justify-between items-center">
             <button className="flex justify-between items-center w-full h-full px-2 py-8">
